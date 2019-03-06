dd5d43b4f2451addecde882aedafe961111353d0
Rohan Kansara <rohankansara1999@gmail.com>
2019-03-07 00:01:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,319 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Welcome!!</title>
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="css/chocolat.css" type="text/css" media="screen">
+<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css'/>
+<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<!-- header -->
+<div class="banner-top">
+			
+			
+			<div class="clearfix"></div>
+		</div>
+	<div class="w3_navigation">
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<div class="navbar-header navbar-left">
+					
+					<h1><a class="navbar-brand" href="index.php"> <span><h1>High Landing</h1></span></a></h1>
+					<br>
+							<br>
+				</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+					<nav class="menu menu--iris">
+						<ul class="nav navbar-nav menu__list">
+						<br>
+							<br>
+							
+							<li class="menu__item"><a href="#rooms" class="menu__link scroll">Rooms</a></li>
+							<li class="menu__item"><a href="#contact" class="menu__link scroll">Contact Us</a></li>
+							
+							
+						</ul>
+					</nav>
+				</div>
+			</nav>
+		</div>
+	</div>
+<!-- //header -->
+		<!-- banner -->
+	<img src="images/1.jpg" alt=" " class="img-responsive">
+	<!-- //banner --> 
+<!--//Header-->
+
+
+
+
+	 <!-- rooms & rates -->
+      <div class="plans-section" id="rooms">
+				 <div class="container">
+				 <h3 class="title-w3-agileits title-black-wthree">Rooms And Rates</h3>
+						<div class="priceing-table-main">
+				 <div class="col-md-3 price-grid">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r1.jpg" alt=" " class="img-responsive" />
+							<h4>Deluxe Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">	
+								<h3><span>Rs. </span>3200</h3>						
+								<a href="reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid ">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r2.jpg" alt=" " class="img-responsive" />
+							<h4>Luxury Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs. </span>2200</h3>
+								<a href="reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid lost">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/r3.jpg" alt=" " class="img-responsive" />
+							<h4>Guest House</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs. </span>1800</h3>
+								<a href="reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/r4.jpg" alt=" " class="img-responsive" />
+							<h4>Single Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-selet">
+								<h3><span>Rs. </span> 1500</h3>
+								<a href="reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+	 <!--// rooms & rates -->
+  <!-- visitors -->
+	
+  <!-- visitors -->
+  <!-- contact -->
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contact Us</h4>
+				<p class="contact-agile2">Sign Up For Our News Letters</p>
+				<form  method="post" name="sentMessage" id="contactForm" >
+					<div class="control-group form-group">
+                        
+                            <label class="contact-p1">Full Name:</label>
+                            <input type="text" class="form-control" name="name" id="name" required >
+                            <p class="help-block"></p>
+                       
+                    </div>	
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Email Address:</label>
+                            <input type="email" class="form-control" name="email" id="email" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    
+                    
+                    <input type="submit" name="sub" value="Send Now" class="btn btn-primary">	
+				</form>
+				<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`) VALUES ('$name','$phone','$email')" ;
+					
+					
+					if(mysqli_query($con,$sql))
+					echo"OK";
+					
+				}
+				?>
+			</div>
+		</div>
+		+
+			
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+
+<!-- contact -->
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div>
+			<h3>Connect With Us</h3>
+			<h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+91-8545728105</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">highlanding@gmail.com</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> Zaveri Bazaar, Opposite Infinity Mall, Borivali West</p>
+			</h4>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+<!-- /contact -->
+			
+<!--/footer -->
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<!-- contact form -->
+<script src="js/jqBootstrapValidation.js"></script>
+
+<!-- gallery popup -->
+<link rel="stylesheet" href="css/swipebox.css">
+				<script src="js/jquery.swipebox.min.js"></script> 
+					<script type="text/javascript">
+						jQuery(function($) {
+							$(".swipebox").swipebox();
+						});
+					</script>
+<!-- //gallery popup -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- flexSlider -->
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			<!-- //flexSlider -->
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						// You can also use "$(window).load(function() {"
+						$(function () {
+						  // Slideshow 4
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							pager:true,
+							nav:false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+					
+						});
+			</script>
+		<!--search-bar-->
+		<script src="js/main.js"></script>	
+<!--//search-bar-->
+<!--tabs-->
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+$(document).ready(function () {
+$('#horizontalTab').easyResponsiveTabs({
+type: 'default', //Types: default, vertical, accordion           
+width: 'auto', //auto or any width like 600px
+fit: true,   // 100% fit in a container
+closed: 'accordion', // Start closed if in accordion view
+activate: function(event) { // Callback function if tab is switched
+var $tab = $(this);
+var $info = $('#tabInfo');
+var $name = $('span', $info);
+$name.text($tab.text());
+$info.show();
+}
+});
+$('#verticalTab').easyResponsiveTabs({
+type: 'vertical',
+width: 'auto',
+fit: true
+});
+});
+</script>
+<!--//tabs-->
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	
+	<div class="arr-w3ls">
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	</div>
+<!-- //smooth scrolling -->
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
+
+
---
+++ print.php
@@ -0,0 +1,29 @@
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Thank You!!</title>
+		
+
+		</style>
+		
+	</head>
+	<body>
+	
+	
+	
+	
+	
+		<header>
+			
+			<address >
+				<center>
+				<p><br><br><object align="middle"><h1>THANK YOU!!</h1>
+				<br><h1>Your booking is successful.</h1>
+				<br><h1>Have an amazing stay.</h1></p>.	</center>
+			
+		</header>
+		
+		
+	</body>
+</html>
